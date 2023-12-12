e8e3b9c5aa619702e84c587fa190a9b7643e35e3
themrleoner12@mail.ru <themrleoner12@mail.ru>
2023-12-12 15:45:11 +0700

    reading cyrillic

--- main.py
+++ main.py
@@ -1,43 +1,36 @@
+
 import tensorflow as tf
-import keras
-import random
-import numpy as np
-import matplotlib.pyplot as plt
-from keras.layers import Dense, Flatten
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from keras.models import Sequential
-from keras.datasets import mnist
+import scipy
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-#нормализация входных данных
-X_train = X_train/255
-X_test = X_test/255
-# преобразование выходных значений в векторы по категориям
-y_train_cat = keras.utils.to_categorical(y_train, 10)
-y_test_cat = keras.utils.to_categorical(y_test, 10)
-
-
-model = Sequential()
-# преобразование изображений в входные данные
-model.add(Flatten(input_shape=(28, 28, 1)))
-
-# скрытый слой
-model.add(Dense(128, activation='relu'))
-
-# выходной слой
-model.add(Dense(10, activation='softmax'))
-
-
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-# validation_split - обучающая выборка=80%, выборка валидации=20%
-model.fit(X_train, y_train_cat, batch_size=32, epochs=5, validation_split=0.2)
-
-k = random.randint(0, 783)
-x = np.expand_dims(X_test[k], axis=0)
-res = model.predict(x)
-print(res)
-plt.text(0.1, 1.1, f"Расспознанная цифра: {np.argmax(res)}")
-plt.imshow(X_test[k], cmap='binary')
-plt.show()
+ImageDataGenerator = tf.keras.preprocessing.image.ImageDataGenerator
+TRAINING_DIR = "Cyrillic"
+train_datagen = ImageDataGenerator(rescale=1.0 / 255.)
+train_generator = train_datagen.flow_from_directory(TRAINING_DIR,
+                              batch_size=40,
+                              class_mode='binary',
+                              target_size=(278,278))
+VALIDATION_DIR = "Cyrillic"
+validation_datagen = ImageDataGenerator(rescale=1.0 / 255.)
+validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
+                                      batch_size=40,
+                                      class_mode='binary',
+                                      target_size=(278,278))
+model = Sequential([
+    Conv2D(16, (3, 3), activation='relu',
+                input_shape=(278,278, 3)),
+    MaxPooling2D(2, 2),
+    Conv2D(32, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+    Flatten(),
+    Dense(512, activation='relu'),
+    Dense(33, activation='softmax')
+])
+model.compile(optimizer='adam',
+loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+history = model.fit(train_generator, epochs=2, verbose=1, validation_data=validation_generator)
+model.save("model.h5")
